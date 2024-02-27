91a3e253bef4b7501bb040f1c3e6f9732f9210dc
MUHAMMAD SHAHID <154737925+shahidkha9@users.noreply.github.com>
2024-02-27 17:06:37 +0500

    Add files via upload

---
+++ class-2.js
@@ -0,0 +1,6 @@
+var myName = "Muhammad Shahid";
+console.log(myName);
+var cellPhone = +9211111111;
+console.log(cellPhone);
+var isTeacher = false;
+console.log(isTeacher);
---
+++ class-2.ts
@@ -0,0 +1,14 @@
+let myName: string = "Muhammad Shahid"
+console.log(myName);
+let cellPhone: number= +9211111111
+console.log(cellPhone);
+let isTeacher:boolean= false
+console.log(isTeacher);
+
+
+
+
+
+
+
+
